57e973019fac94d45cd198a0514daf0184d933be
lia1810 <lbelliski@gmail.com>
2019-12-19 01:32:30 -0300

    emprolijé un poco

--- ejercicio-integrador.js
+++ ejercicio-integrador.js
@@ -1,24 +1,43 @@
-// Para ordenarme mentalmente
-// Tenemos un array con los productos
-// Tenemos un array vacio sobre el que tenemos que aplicar los elementos del primer array a demanda del usuario
-// Tenemos un menu que da las opciones para acccionar
+// const mostrarDetalle = () => {
 
+// }
 
+// const eliminarProducto = () => {
+
+// }
+// const vaciarCarrito = () => {
+
+// }
+// const confirmarCompra = () => {
+
+// }
+// const cancelarCompra = () => {
+
+// }
+// const contarTotalDeProductos = () => { //recibe el array del carrito de compras y tiene que retornar la cantidad de productos en el carrito
+
+// }
+// const subtotalDeLaCompra = () => { //recibe el array del carrito de compras y tiene que retornar el monto total del carrito
+
+// }
+// const totatlDescuento = () => { //recibe el array del carrito de compras y tiene que retornar el monto total del descuento que aplica.
+//     //  Como no todos los productos aplican para descuento, solo se debe calcular sobre el subtotal de los productos que si aplican
+
+// }
+// const mostrarProductosdelCarrito = () => { //recibe el array del carrito de compras y muestra el listado de productos con los siguientes datos: nombre del producto, precio, cantidad y subtotal (precio x cantidad)
+
+// }
 
 
 let productosAlaVenta = [
-
     [1, "Notebook Lenobo S400", 100, "si"],
     [2, "Celular Notorola G5", 135, "no"],
     [3, "Smart TV Filis 43'", 190, "si"],
     [4, "Sorny PS 7", 215, "si"]
-
 ]
-
 let carritoDeCompra = [];
 
 //Bienvenida
-
 
 const catalogo = () => {
     let mostrarProductos = "";
@@ -29,253 +48,79 @@ const catalogo = () => {
     }
     return mostrarProductos
 }
-alert(`Bienvenido a nuestra tienda, estos son nuestros productos 
+alert(`Bienvenido a nuestra tienda, estos son nuestros productos
     ${catalogo()}`)
+
 //Opciones para el cliente
-
-
+l
 let volverAlMenuPrincipal = "SI"
 while (volverAlMenuPrincipal == "SI") {
-    accion = prompt(`✅ Seleccione una operacion 
+    accion = prompt(`Seleccione una operacion:
       --------------------------
-      ➡️ [AGREGAR] un producto
-      ➡️ [MOSTRAR] detalle 📄
-      ➡️ [ELIMINAR] un producto ❌ 
-      ➡️ [VACIAR] carrito
-      ➡️ [CONFIRMAR] compra
-      ➡️ [CANCELAR] compra 👋`);
-
+     [AGREGAR] un producto
+     [MOSTRAR] detalle :página_boca_arriba:
+     [ELIMINAR] un producto 
+     [VACIAR] carrito
+     [CONFIRMAR] compra
+     [CANCELAR] compra `);
     volverAlMenuPrincipal = volverAlMenuPrincipal.toUpperCase()
     accion = accion.toUpperCase()
-
-
     //     //...............AGREGAR productos al carrito..............
-    //Primero tenemos que conseguir meter los productos en el carrito. Tantos como el cliente quiera y si desea agregar mas sumarlos a los existentes
-    // Y entonces empezamos a trabajar con el array carrito
-    //Mientras el cliente siga haciendo cosas tiene que volver al menu inicial
+
     const agregarAlCarrito = () => {
         let productoAAgregar = prompt(`${catalogo()}
     Indique el id del productor que desea agregar al carrito`);
-    /*
-                Si el producto existe, preguntar cuantas unidades va a llevar del producto y agregarlo al carrito
-                Si el producto ya se encontraba en el carrito, debe incrementar la cantidad de unidades que está comprando
-                */
 
-    let idValido = false;
-    for (let i = 0; i < productosAlaVenta.length; i++) {
-        for (let j = 0; j < productosAlaVenta[i].length; j++) {
-
-            if (productoAAgregar == productosAlaVenta[i][j]) {
-                let cantidadAAgregar = prompt("Cuántas unidades desea agregar?")
-                idValido = true
-
-                if (carritoDeCompra.length == 0) {
-                    carritoDeCompra.push(productosAlaVenta[i])
-                    carritoDeCompra[0][4] = cantidadAAgregar
-                }
-                else {
-                    for (let i = 0; i < carritoDeCompra.length; i++) {
-                        if (carritoDeCompra[i][0] == productoAAgregar) {
-                            carritoDeCompra[i][4] += cantidadAAgregar
-                        }
-                        else {
-                            carritoDeCompra.push(productosAlaVenta[i])
-                            carritoDeCompra[i][4].push(cantidadAAgregar)
+        let idValido = false;
+        for (let i = 0; i < productosAlaVenta.length; i++) {
+            for (let j = 0; j < productosAlaVenta[i].length; j++) {
+                if (productoAAgregar == productosAlaVenta[i][j]) {
+                    let cantidadAAgregar = prompt("Cuántas unidades desea agregar?")
+                    cantidadAAgregar = parseInt(cantidadAAgregar)
+                    idValido = true
+                    if (carritoDeCompra.length == 0) {
+                        carritoDeCompra.push(productosAlaVenta[i])
+                        carritoDeCompra[0][4] = cantidadAAgregar
+                    } else {
+                        for (let i = 0; i < carritoDeCompra.length; i++) {
+                            if (carritoDeCompra[i][0] == productosAlaVenta[i]) {
+                                // 62 era carritoDeCompra[i][0] == productosAlaVenta
+                                carritoDeCompra[i][4] += cantidadAAgregar
+                            }
+                            //else {
+                            //carritoDeCompra.push(productoAAgregar[i])
+                            //carritoDeCompra[i][4].push(cantidadAAgregar)
+                            // No sé bien por qué, pero me tiraba error en la linea 67
+                            // La dejo comentada porque así funciona pero no entiendo
+                            // y quiero entender
+                            //}
                         }
                     }
                 }
             }
+            console.log(`Carrito de compra: ${carritoDeCompra}`)
+        }
+        if (idValido === false) {
+            alert("Ingrese un id valido")
         }
     }
-    if (idValido === false) {
-        alert("Ingrese un id valido")
-    }
-    console.log(`Carrito de compra: ${carritoDeCompra}`)
-
-    }
     if (accion == "AGREGAR") {
-        //         let repetirOperacion = "SI";
-
+        //let repetirOperacion = "SI";
         agregarAlCarrito()
-
-        const repetirOperacion = (str) => {
-            let confirmacion = prompt("Desea repetir la operacion?") 
-            if (confirmacion === "NO") {
-                volverAlMenuPrincipal = "SI";
-            } else if (confirmacion !== "SI") {
-    
-                alert(`
-                🚫 Opción inválida
-                🙏 Por favor, ingresar una opción correcta`);
-            } else {
-                agregarAlCarrito()
-            }
-
-
-        // }
     }
-    volverAlMenuPrincipal = "no"
+    const repetirOperacion = (str) => {
+        let confirmacion = prompt("Desea repetir la operacion?")
+        if (confirmacion === "NO") {
+            volverAlMenuPrincipal = "SI";
+        } else if (confirmacion !== "SI") {
+            alert(`
+                 Opción inválida: Por favor, ingresar una opción correcta`);
+            repetirOperacion()
+        } else {
+            agregarAlCarrito()
+            repetirOperacion()
+        }
+        volverAlMenuPrincipal = "no"
+    }
+    repetirOperacion()
 }
-
-// //         while (repetirOperacion == "SI") {
-// //             nombreProducto = prompt(" 📓 Ingrese el nombre del usuario");
-// //             telefono = prompt(" 📞 Ingrese el teléfono del usuario");
-// //             email = prompt(" 📫 Ingrese email del usuario");
-// //             let nuevoUsuario = [id, nombre, telefono, email];
-// //             let respuesta = prompt(`Los datos del usuario ingresado son:
-// //           ID: ${id}    
-// //           NOMBRE: ${nombre} 
-// //           TELEFONO: ${telefono} 
-// //           EMAIL: ${email}
-// //           Desea confirmar SI -NO ?
-// //           `);
-// //             respuesta = respuesta.toUpperCase()
-// //             if (respuesta == "SI") {
-// //                 productosAlaVenta.push(nuevoUsuario);
-// //                 alert("El usuario fue guardado exitosamente!")
-// //             } else {
-// //                 alert("Los cambios no han sido guardadosse cancelo la operacion.");
-// //             }
-// //             repetirOperacion = prompt(`Desea agregar otro usuario? (SI-NO)`);
-// //             repetirOperacion = repetirOperacion.toUpperCase()
-// //         }
-
-
-// //     }
-
-// //     if (accion == "LISTAR") {
-// //         let mostrarUsuarios = "";
-// //         for (let i = 0; i < productosAlaVenta.length; i++) {
-// //             mostrarUsuarios = mostrarUsuarios +
-// //                 `ID: ${productosAlaVenta[i][0]};  NOMBRE: ${productosAlaVenta[i][1]};  TELEFONO: ${productosAlaVenta[i][2]};  MAIL: ${productosAlaVenta[i][3]}
-// // `;
-// //         }
-// //         alert(mostrarUsuarios);
-
-// //     }
-
-// //     if (accion == "ELIMINAR") {
-// //         let repetirOperacion = "SI";
-// //         repetirOperacion = repetirOperacion.toUpperCase()
-// //         let usuarioEncontrado = false;
-// //         while (repetirOperacion == "SI") {
-// //             let usuarioAEliminar = prompt("Indique el ID del usuario que desea eliminar");
-// //             for (i = 0; i < productosAlaVenta.length; i++) {
-// //                 if (productosAlaVenta[i][0] == usuarioAEliminar) {
-// //                     confirmarOperacion = prompt(`Los datos del usuario a eliminar son:
-// //                   ID: ${productosAlaVenta[i][0]}
-// //                   NOMBRE: ${productosAlaVenta[i][1]}
-// //                   TELEFONO: ${productosAlaVenta[i][2]}
-// //                   MAIL: ${productosAlaVenta[i][3]}
-// //                   Desea confirmar? (SI-NO)
-// //                   `)
-// //                     if (confirmarOperacion == "SI") {
-// //                         productosAlaVenta.splice(i, 1);
-// //                         alert("La operacion ha sido realizada exitosamente");
-// //                     } else {
-// //                         alert("La operacion ha sido cancelada")
-// //                     }
-// //                 }
-// //             }
-// //         }
-// //     }
-
-// //     if (accion == "SALIR") {
-// //         let confirma = prompt('Confirma que quiere salir del programa SI - NO?')
-// //         confirma = confirma.toUpperCase()
-// //         if (confirma == "SI") {
-// //             alert('Muchas gracias por su atención')
-// //             volverAlMenuPrincipal = "NO"
-// //         } else {
-// //             volverAlMenuPrincipal = "SI"
-// //         };
-// //     };
-
-// // }
-
-//Lo de Matias
-
-//let productosAlaVenta = [
-
-//     [1, "Notebook Lenobo S400", 100, "si"],
-//     [2, "Celular Notorola G5", 135, "no"],
-//     [3, "Smart TV Filis 43'", 190, "si"],
-//     [4, "Sorny PS 7", 215, "si"]
-
-// ]
-
-// let carritoDeCompra = [];
-
-// //Bienvenida
-
-// const listarProductos = () =>{
-//   let mostrarProductos = "";
-//   for (let i = 0; i < productosAlaVenta.length; i++) {
-//       mostrarProductos = mostrarProductos +
-//           `ID: ${productosAlaVenta[i][0]};  NOMBRE: ${productosAlaVenta[i][1]};  PRECIO: ${productosAlaVenta[i][2]};  APLICA DESCUENTO: ${productosAlaVenta[i][3]}
-//   `;
-//   }
-
-//   return mostrarProductos
-
-// }
-
-
-// //Opciones para el cliente
-//   alert(`Bienvenido a nuestra tienda, estos son nuestros productos 
-//       ${listarProductos()}`);
-
-// let volverAlMenuPrincipal = "SI";
-// let action = "";
-// while (volverAlMenuPrincipal == "SI") {
-//    let accion = prompt(`✅ Seleccione una operacion 
-//       --------------------------
-//       ➡️ [AGREGAR] un producto
-//       ➡️ [MOSTRAR] detalle 📄
-//       ➡️ [ELIMINAR] un producto ❌ 
-//       ➡️ [VACIAR] carrito
-//       ➡️ [CONFIRMAR] compra
-//       ➡️ [CANCELAR] compra 👋`);
-
-//     volverAlMenuPrincipal = volverAlMenuPrincipal.toUpperCase()
-//     accion = accion.toUpperCase()
-
-
-// //     //...............AGREGAR productos al carrito..............
-// //Primero tenemos que conseguir meter los productos en el carrito. Tantos como el cliente quiera y si desea agregar mas sumarlos a los existentes
-// // Y entonces empezamos a trabajar con el array carrito
-// //Mientras el cliente siga haciendo cosas tiene que volver al menu inicial
-//     if (accion == "AGREGAR") {
-// //         let repetirOperacion = "SI";
-//         let productoAAgregar = prompt(`${listarProductos()}
-//         Indique el id del productor que desea agregar al carrito`);
-// /*
-//             Si el producto existe, preguntar cuantas unidades va a llevar del producto y agregarlo al carrito
-//             Si el producto ya se encontraba en el carrito, debe incrementar la cantidad de unidades que está comprando
-//             */
-//         let idValido = false;
-
-//         for (producto of productosAlaVenta) {
-//             if (productoAAgregar == producto[0]) {
-//                     let cantidadAAgregar = prompt("Cuántas unidades desea agregar?");
-//                     let noExisteEnCarrito = true;
-//                     for(const [index,item] of carritoDeCompra){
-//                       if(item[0] == productoAAgregar){
-//                         carritoDeCompra[index][4] += cantidadAAgregar;
-//                         noExisteEnCarrito= false
-//                       }
-//                     }
-//                     if(noExisteEnCarrito){
-//                       let item  = producto;
-//                       item[4] = parseInt(cantidadAAgregar)
-//                       carritoDeCompra.push(item);
-//                     }
-//                     idValido = true;
-//                 }
-//             }
-//      console.log(carritoDeCompra)
-//         }
-//         action = ""
-//         volverAlMenuPrincipal = "";
-
-// }
